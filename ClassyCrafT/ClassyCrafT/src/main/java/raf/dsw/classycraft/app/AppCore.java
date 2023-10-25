27ad0d9ee85eee64babbb06c8f97760dec087c9b
milica <mjocic10723rn@raf.rs>
2023-10-25 23:29:06 +0200

    Nesto pokusavam

--- ClassyCrafT/ClassyCrafT/src/main/java/raf/dsw/classycraft/app/AppCore.java
+++ ClassyCrafT/ClassyCrafT/src/main/java/raf/dsw/classycraft/app/AppCore.java
@@ -5,6 +5,6 @@ import raf.dsw.classycraft.app.core.ApplicationFramework;
 public class AppCore {
     public static void main(String[] args) {
         ApplicationFramework appCore = ApplicationFramework.getInstance();
-        appCore.initialize();
+        appCore.initialize(); //
     }
 }
