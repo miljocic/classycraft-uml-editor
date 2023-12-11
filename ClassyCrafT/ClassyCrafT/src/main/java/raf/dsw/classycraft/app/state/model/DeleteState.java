79d80cae0d6e137c28a82ba0648ff5c1beabb396
Jovan Spasojevic <jspasojevic4621rn@raf.rs>
2023-12-11 16:04:15 +0100

    Doradjen Delete da kad se obrise element koji je u vezi, da se brise i veza

--- ClassyCrafT/ClassyCrafT/src/main/java/raf/dsw/classycraft/app/state/model/DeleteState.java
+++ ClassyCrafT/ClassyCrafT/src/main/java/raf/dsw/classycraft/app/state/model/DeleteState.java
@@ -1,13 +1,18 @@
 package raf.dsw.classycraft.app.state.model;
 
 import raf.dsw.classycraft.app.gui.swing.view.DiagramView;
+import raf.dsw.classycraft.app.gui.swing.view.painters.ConnectionPainter;
 import raf.dsw.classycraft.app.gui.swing.view.painters.ElementPainter;
+import raf.dsw.classycraft.app.gui.swing.view.painters.InterclassPainter;
 import raf.dsw.classycraft.app.repository.implementation.Diagram;
+import raf.dsw.classycraft.app.repository.implementation.connectionElements.Connection;
 import raf.dsw.classycraft.app.state.State;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 
 public class DeleteState implements State {
@@ -18,13 +23,33 @@ public class DeleteState implements State {
 
         Diagram diagram = dV.getDiagram();
         Point pos = e.getPoint();
+        ElementPainter deleted = null;
         Iterator<ElementPainter> it = dV.getElementPainters().iterator();
         while(it.hasNext()) {
-            ElementPainter elementView = it.next();
-            if(elementView.elementAt(pos)) {
-                if(elementView.equals(dV.getSelected())) dV.setSelected(null);
-                diagram.deleteChild(elementView.getElement());
+            ElementPainter elementPainter = it.next();
+            if(elementPainter.elementAt(pos)) {
+                if(elementPainter.equals(dV.getSelected())) dV.setSelected(null);
+                diagram.deleteChild(elementPainter.getElement());
+                deleted = elementPainter;
                 break;
+            }
+        }
+
+        if(deleted instanceof InterclassPainter) {
+            List<Connection> connectionList = new ArrayList<>();
+            it = dV.getElementPainters().iterator();
+            while (it.hasNext()) {
+                ElementPainter elementPainter = it.next();
+                if(elementPainter instanceof ConnectionPainter) {
+                    ConnectionPainter connectionPainter = (ConnectionPainter) elementPainter;
+                    Connection connection = (Connection) connectionPainter.getElement();
+                    if(connection.getTo().equals(deleted.getElement()) || connection.getFrom().equals(deleted.getElement())) {
+                        connectionList.add(connection);
+                    }
+                }
+            }
+            for(Connection c : connectionList) {
+                diagram.deleteChild(c);
             }
         }
 
